e808e638f1ceef2f54e560acca12b29d54b3617c
PedroRicordi <131784634+PedroRicordi@users.noreply.github.com>
2023-12-21 11:54:52 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function setup() {
+    canvas = createCanvas(640, 480); 
+    canvas.position(110, 250); 
+    video = createCapture(VIDEO); 
+    video.hide(); 
+    
+    tint_color = ""; 
+   } 
